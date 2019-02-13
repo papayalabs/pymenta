4ad83134e1312f0281a1183e78ce2c165164c251
Papaya Labs <papayalabs@gmail.com>
2019-02-13 12:33:27 +0100

    Use amazon s3 for images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,17 +85,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   # Configuration for Paper Clip
-  #config.paperclip_defaults = {
-  #:storage => :s3,
-  #:s3_credentials => {
-  #  :bucket => ENV['S3_BUCKET_NAME'],
-  #  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #  :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  #  :path => '/:class/:attachment/:id/:style_:basename.:extension',
-  #  :url => ":s3_domain_url",
-  #  :s3_host_name => "s3.amazonaws.com"
-  #  }
-  #}
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :path => '/:class/:attachment/:id/:style_:basename.:extension',
+    :url => ":s3_domain_url",
+    :s3_host_name => "s3.amazonaws.com"
+    }
+  }
   
   # Configuration for mailer
   config.action_mailer.smtp_settings = {
