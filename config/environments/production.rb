9c677d26d927aea258ba5eb88fe7ce8b0fed4ba8
Manuel Fernandez <papayalabs@gmail.com>
2021-09-09 09:20:08 -0400

    Put comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
     :s3_region => 'us-east-1',
     :s3_host_name => ENV["S3_HOST_NAME"],
     :bucket => 'pymenta',
-    :endpoint => ENV["S3_ENDPOINT"]
+    :endpoint => ENV["S3_ENDPOINT"],
     :s3_protocol => :https,
     :s3_credentials => "#{Rails.root}/config/aws.yml"
   }
